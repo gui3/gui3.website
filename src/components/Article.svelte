cd46719fe2dc0038da60b1f908b1149235518ad6
gui3 <gui.silvent@gmail.com>
2020-06-10 19:29:17 +0200

    articles in about

---
+++ src/components/Article.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let image = null
+  export let alt = null
+</script>
+
+<style>
+  article img {
+    -height: 100%;
+    max-width: 50%;
+    float: left;
+  }
+</style>
+
+<article>
+  {#if image}
+    <img src={image} alt={alt}>
+  {/if}
+  <slot/>
+</article>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,18 +1,19 @@
 <script>
   import Panel from '../components/Panel.svelte'
+  import Article from '../components/Article.svelte'
 </script>
 
-<div>
-  <h1>About this site</h1>
+<Panel>
+  <Article>
+    <h1>About this site</h1>
 
-  <p>This is the 'about' page. There's not much here.</p>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-  <hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/>
-</div>
+    <p>This website uses these technologies :</p>
+  </Article>
+
+  <Article image={'images/logos/Svelte.png'} alt={'logo Svelte'}>
+    <h2>Svelte + Sapper</h2>
+    <p>
+      bla bla bla
+    </p>
+  </Article>
+</Panel>
